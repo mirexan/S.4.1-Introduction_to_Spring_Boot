d5d40c8183303e052d9fe868056c12139aed90f0
¨Mirexan¨ <mmregada@gmail.com>
2025-12-02 19:46:26 +0100

    test: /users endpoints unitary Tests

--- src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
@@ -1,24 +1,93 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.models.NewUserDTO;
+import cat.itacademy.s04.t01.userapi.models.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
-	@Test
-	public void shouldReturnEmptyUserList()throws Exception{
-	mockMvc.perform(get("/users"))
-			.andExpect(status().isOk())
-			.andExpect(content().json("[]"));
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@BeforeEach
+	void setUp() {
+		if (UserController.userList != null) {
+			UserController.userList.clear();
+		}
 	}
 
+	@Test
+	public void getUsers_returnsEmptyListInitially() throws Exception {
+		mockMvc.perform(get("/users"))
+				.andExpect(status().isOk())
+				.andExpect(content().json("[]"));
+	}
+
+	@Test
+	public void createUser_returnsUserWithId() throws Exception {
+		NewUserDTO newUser = new NewUserDTO("Paquita", "PSalas@PSManagement.com");
+		String newUserJson = objectMapper.writeValueAsString(newUser);
+		mockMvc.perform(post("/users")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(newUserJson))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").isNotEmpty())
+				.andExpect(jsonPath("$.name").value("Paquita"));
+	}
+
+	@Test
+	void getUserById_returnsCorrectUser() throws Exception {
+		NewUserDTO newUser = new NewUserDTO("Magwi", "MagMartinez@PSManagement.com");
+		String answer = mockMvc.perform(post("/users")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(newUser)))
+						.andExpect(status().isOk())
+						.andReturn().getResponse().getContentAsString();
+		User user = objectMapper.readValue(answer, User.class);
+		mockMvc.perform(get("/users/" + user.id().toString()))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("Magwi"))
+				.andExpect(jsonPath("$.id").value(user.id().toString()));
+	}
+
+	@Test
+	void getUserById_returnsNotFoundIfMissing() throws Exception {
+		mockMvc.perform(get("/users/" + UUID.randomUUID().toString()))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	void getUsers_withNameParam_returnsFilteredUsers() throws Exception {
+		createNewTestUser("Paquita", "PSalas@PSManagement.com");
+		createNewTestUser("Magwi", "MagMartinez@PSManagement.com");
+		mockMvc.perform(get("/users?name=Paquita"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].name").value("Paquita"))
+				.andExpect(jsonPath("$",hasSize(1)));
+	}
+	private void createNewTestUser(String name, String email) throws Exception {
+		NewUserDTO newUser = new NewUserDTO(name, email);
+		mockMvc.perform(post("/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(newUser)));
+	}
 }
